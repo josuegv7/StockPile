a32d9609bba63a29c8b589ffbabcb231122a7f7d
Josue Guzman <josueguzman@Josues-MacBook-Pro-2.local>
2018-04-03 17:25:59 -0400

    heroku update

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ app.use(cors())
 app.use(bodyParser.json({type: '*/*'}))
 router(app)
 
+if (process.env.NODE_ENV === 'production'){
+    // express will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
 // SERVER SETUP:
 const port = process.env.Port || 4000
 const server = http.createServer(app)
