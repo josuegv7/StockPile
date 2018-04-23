6a15f63ebfdc538a86bee669876f0693c89ac958
Josue Guzman <josueguzman@Josues-MacBook-Pro-2.local>
2018-04-23 17:12:50 -0400

    got rid of RootUrl from acion file

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,15 @@
 import Axios from 'axios';
 import {AUTH_USER, AUTH_ERROR, UNAUTH_USER, FETCH_FOODLIST, ADD_NEW_FOOD, DELETE_FOOD, ADD_INGREDIENT} from './types';
-const ROOT_URL = 'http://localhost:4000';
+// const ROOT_URL = 'http://localhost:4000';
+
+
 
 
 // Action for Authentication: sigin/signout/signup
 export function signinUser({ email, password }, history) {
     return function (dispatch) {
         // submit email/password to the server
-        Axios.post(`${ROOT_URL}/signin`, { email, password })
+        Axios.post('/signin', { email, password })
             .then( response => {
                 // IF request is good...
                 // update state to indicate user is authenticated
@@ -28,7 +30,7 @@ export function signinUser({ email, password }, history) {
 
 export function signupUser({ email, password }, history) {
     return function(dispatch) {
-        Axios.post(`${ROOT_URL}/signup`, {email, password})
+        Axios.post('/signup', {email, password})
             .then(response => {
                 dispatch({type: AUTH_USER });
                 localStorage.setItem('token', response.data.token);
@@ -55,7 +57,7 @@ export function authError(error) {
 // Action to get the food list:
 export const fetchFoodList = () => dispatch => {
     Axios
-        .get(`${ROOT_URL}/myfoodlist`, {
+        .get('/myfoodlist', {
             headers: { auth: localStorage.getItem('token') }
         })
         .then( response =>
@@ -68,7 +70,7 @@ export const fetchFoodList = () => dispatch => {
 // Add a food to the DB:
 export function addFood (values) {
     return function (dispatch) {
-        Axios.post(`${ROOT_URL}/myfoodlist/foods`, values)
+        Axios.post('/myfoodlist/foods', values)
             .then( response => {
                 dispatch ({
                     type: ADD_NEW_FOOD,
@@ -96,7 +98,7 @@ export const addToPot = (ingredient) => {
 // Delete food from Stock:
 export function deleteFood ( id, history) {
     return function (dispatch) {
-        Axios.delete(`${ROOT_URL}/myfoodlist/foods/${id}`)
+        Axios.delete(`/myfoodlist/foods/${id}`)
             .then ( response => {
                 dispatch({
                 type: DELETE_FOOD,
