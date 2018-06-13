b37a2a4076e9d79c4316eaf8f9144bbeb4a978e2
Josue Guzman <josueguzman@Josues-MacBook-Pro-2.local>
2018-06-12 23:05:01 -0400

    keys

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     secret: process.env.SECRET,
-    ApplicationID: process.env.APPLICATIONID
-    YUMMLY_API_ID: process.env.YUMMLYAPIID
-    YUMMLY_API_KEY:process.env.YUMMLYAPIKEY
+    ApplicationID: process.env.APPLICATIONID,
+    YUMMLYAPIID: process.env.YUMMLYAPIID,
+    YUMMLYAPIKEY:process.env.YUMMLYAPIKEY
  };
